9ecb09db0bf6d2c4694111f593fdadb3384c66d7
Kirill Korepanov <xorboo@gmail.com>
2015-12-19 18:54:31 +0300

    Player improvements

---
+++ Assets/Scripts/Player/PlayerObjectsDisabler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerObjectsDisabler : NetworkBehaviour
+{
+    [SerializeField]
+    bool DeleteInsteadOfDisable = false;
+    [SerializeField]
+    List<GameObject> DisableSelf = null;
+    [SerializeField]
+    List<GameObject> DisableOthers = null;
+
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            foreach (var obj in DisableSelf)
+            {
+                if (DeleteInsteadOfDisable)
+                    Destroy(obj);
+                else
+                    obj.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (var obj in DisableOthers)
+            {
+                if (DeleteInsteadOfDisable)
+                    Destroy(obj);
+                else
+                    obj.SetActive(false);
+            }
+        }
+    }
+}
